08919e1f581aa2005ed866617cdf3690ccca4259
zty1205 <zty1160051490@163.com>
2018-05-06 23:28:33 +0800

    路由重构

--- src/main.js
+++ src/main.js
@@ -3,23 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import store from './store/store'
-import axios from 'axios'
 import VueLazyload from 'vue-lazyload'
 // 引入配置的router文件
 import router from './router';
-import IView from 'iview';
+import iView from 'iview';
 import 'iview/dist/styles/iview.css';
 
 
-Vue.use(IView);
+Vue.use(iView);
 
 Vue.config.productionTip = false
-Vue.prototype.$http = axios
+
 // 懒加载图片 在图片为加载完成前 先显示这个图片
 Vue.use(VueLazyload, {
   loading: require('./assets/img/placeholder.png')
 })
-
 
 /* eslint-disable no-new */
 new Vue({
