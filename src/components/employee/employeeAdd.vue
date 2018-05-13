d6b169f2f4770f4516c20a5bde52df51752bc033
zty1205 <zty1160051490@163.com>
2018-05-13 15:16:54 +0800

    图片假上传

--- src/components/employee/employeeAdd.vue
+++ src/components/employee/employeeAdd.vue
@@ -112,10 +112,47 @@
       </FormItem>
 
       <!--图片和头像 先不考虑-->
+      <FormItem style="margin-top: 60px">
+        <label class="photo-label">头像</label>
+        <div class="demo-upload-list" v-for="item in uploadList">
+          <template v-if="item.status === 'finished'">
+            <img :src="item.url">
+            <div class="demo-upload-list-cover">
+              <Icon type="ios-eye-outline" @click.native="handleView(item.name)" size="30"></Icon>
+              <Icon type="ios-trash-outline" @click.native="handleRemove(item)" size="30"></Icon>
+            </div>
+          </template>
+          <template v-else>
+            <Progress v-if="item.showProgress" :percent="item.percentage" hide-info></Progress>
+          </template>
+        </div>
+        <Upload
+          ref="upload"
+          :show-upload-list="false"
+          :default-file-list="defaultList"
+          :on-success="handleSuccess"
+          :format="['jpg','jpeg','png']"
+          :max-size="2048"
+          :on-format-error="handleFormatError"
+          :on-exceeded-size="handleMaxSize"
+          :before-upload="handleBeforeUpload"
+          multiple
+          type="drag"
+          action="//jsonplaceholder.typicode.com/posts/"
+          style="display: inline-block;width:158px;">
+          <div style="width: 158px;height:158px;line-height: 158px;">
+            <Icon type="camera" size="100" style="margin-top: 30px;"></Icon>
+          </div>
+        </Upload>
+        <Modal title="View Image" v-model="visible">
+          <img :src="'https://o5wwk8baw.qnssl.com/' + imgName + '/large'" v-if="visible" style="width: 100%">
+        </Modal>
+      </FormItem>
+      <!---->
 
       <div class="form-btn">
         <Button type="primary" @click="handleSubmit('employeeForm')">Submit</Button>
-        <Button type="ghost" @click="handleSubmit('employeeForm')" style="margin-left: 8px">Cancel</Button>
+        <Button type="ghost" @click="handleSubmit('employeeForm')" style="margin-left: 18px">Cancel</Button>
       </div>
 
     </Form>
@@ -138,8 +175,26 @@
           time: null,
           department: null,
           role: ''
-        }
+        },
+        // 图片 上传
+        defaultList: [
+          {
+            'name': 'a42bdcc1178e62b4694c830f028db5c0',
+            'url': 'https://o5wwk8baw.qnssl.com/a42bdcc1178e62b4694c830f028db5c0/avatar'
+          },
+          {
+            'name': 'bc7521e033abdd1e92222d733590f104',
+            'url': 'https://o5wwk8baw.qnssl.com/bc7521e033abdd1e92222d733590f104/avatar'
+          }
+        ],
+        imgName: '',
+        visible: false,
+        uploadList: []
       }
+    },
+    mounted () {
+      // 初始化上传
+      this.uploadList = this.$refs.upload.fileList;
     },
     computed: {
       authorization(){
@@ -152,6 +207,40 @@
       },
       handleReset (name) {
         this.$refs[name].resetFields();
+      },
+      // 图片上传 处理
+      handleView (name) {
+        this.imgName = name;
+        this.visible = true;
+      },
+      handleRemove (file) {
+        const fileList = this.$refs.upload.fileList;
+        this.$refs.upload.fileList.splice(fileList.indexOf(file), 1);
+      },
+      handleSuccess (res, file) {
+        file.url = 'https://o5wwk8baw.qnssl.com/7eb99afb9d5f317c912f08b5212fd69a/avatar';
+        file.name = '7eb99afb9d5f317c912f08b5212fd69a';
+      },
+      handleFormatError (file) {
+        this.$Notice.warning({
+          title: 'The file format is incorrect',
+          desc: 'File format of ' + file.name + ' is incorrect, please select jpg or png.'
+        });
+      },
+      handleMaxSize (file) {
+        this.$Notice.warning({
+          title: 'Exceeding file size limit',
+          desc: 'File  ' + file.name + ' is too large, no more than 2M.'
+        });
+      },
+      handleBeforeUpload () {
+        const check = this.uploadList.length < 5;
+        if (!check) {
+          this.$Notice.warning({
+            title: 'Up to five pictures can be uploaded.'
+          });
+        }
+        return check;
       }
     }
   }
@@ -168,5 +257,51 @@
   .add-form-label {
     font-size: 12px;
     color: #495060;
+  }
+.form-btn {
+  margin-top: 60px;
+}
+.photo-label {
+  font-size: 30px;
+  margin-right: 40px;
+  margin-left: 20px;
+  float: left;
+}
+  /* 图片上传相关 */
+  .demo-upload-list{
+    display: inline-block;
+    width: 160px;
+    height: 160px;
+    text-align: center;
+    line-height: 160px;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    overflow: hidden;
+    background: #fff;
+    position: relative;
+    box-shadow: 0 1px 1px rgba(0,0,0,.2);
+    margin-right: 4px;
+  }
+  .demo-upload-list img{
+    width: 100%;
+    height: 100%;
+  }
+  .demo-upload-list-cover{
+    display: none;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: rgba(0,0,0,.6);
+  }
+  .demo-upload-list:hover .demo-upload-list-cover{
+    display: block;
+  }
+  .demo-upload-list-cover i{
+    color: #fff;
+    font-size: 20px;
+    cursor: pointer;
+    margin: 0 2px;
   }
 </style>
